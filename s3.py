1984d5fe55bca6f63f4e5e9b85f5c7b64eec71c1
dustinthegreat <dustinroberts96@gmail.com>
2021-03-25 18:13:24 -0400

    Fixing mistakes

--- s3.py
+++ s3.py
@@ -4,31 +4,31 @@ import sys
 
 s3client = boto3.client('s3')
 
-class S3Tool():
+class S3Tool:
     def __init__(self):
-        self.
-    def change_client(self, ID, SECRET)
+        pass
+    def change_client(self, ID, SECRET):
         self.client = boto3.client(
             's3',
             # Hard coded strings as credentials, not recommended.
             aws_access_key_id=ID,
             aws_secret_access_key=SECRET
         )
-    def create_bucket():
+    def create_bucket(self):
         bucket_name = 'python-sdk-sample-{}'.format(uuid.uuid4())
         print('Creating new bucket with name: {}'.format(bucket_name))
         s3client.create_bucket(Bucket=bucket_name)
 
-    def list_buckets():
+    def list_buckets(self):
         list_buckets_resp = s3client.list_buckets()
         for bucket in list_buckets_resp['Buckets']:
             print(bucket)
             # if bucket['Name'] == bucket_name:
             #     print('(Just created) --> {} - there since {}'.format(
-            #         bucket['Name'], bucket['CreationDate']))
+            #         bucket`['Name'], bucket['CreationDate']))
 
-    def main():
-        list_buckets()
+    def main(self):
+        self.list_buckets()
 
 
 if __name__ == "__main__":
